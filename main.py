20162054b2e8be10fec3ec4b90e39b067475fbba
DeniceMorones <denice.rico4211@gmail.com>
2024-10-21 18:22:26 -0600

    [feature]: ajuste de tiempo y casilla de salida

--- main.py
+++ main.py
@@ -17,25 +17,25 @@ canvas.pack()
 laberinto = np.array([
     [0, 0, 0, 1, 0, 0, 3, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
     [1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1],
-    [0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 2, 1],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
     [0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1],
-    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1],
-    [1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1],
-    [0, 0, 0, 0, 0, 0, 4, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
-    [0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1],
-    [0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0],
+    [1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0],
     [1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1],
     [0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 111, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1],
     [0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1],
     [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1],
     [1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1],
-    [0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
-    [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1],
-    [0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1],
-    [0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 4 , 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0],
+    [0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+    [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1],
+    [0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 2],
 ])
 
 colores = {0: "white", 1: "black", 2: "blue", 3: "yellow", 4: "green", 111: "orange"}
@@ -64,10 +64,14 @@ def manejar_teletransporte():
 
 def manejar_trivia():
     respuesta = simpledialog.askstring("Trivia", "¿Cuál es la capital de Francia?")
-    if respuesta.lower() == "parís":
+    if respuesta.lower() == "paris":
         messagebox.showinfo("Correcto", "¡Respuesta correcta!")
     else:
         messagebox.showinfo("Incorrecto", "Respuesta incorrecta. ¡Sigue intentándolo!")
+
+
+pos_salida = np.where(laberinto == 2)
+fila_salida, columna_salida = pos_salida[0][0], pos_salida[1][0]
 
 def mover_jugador_auto():
     posibles_direcciones = []
@@ -82,26 +86,35 @@ def mover_jugador_auto():
     if posibles_direcciones:
         nueva_fila, nueva_columna = random.choice(posibles_direcciones)
     else:
-        while True:
-            direccion = random.choice(direcciones)
-            nueva_fila = jugador_pos[0] + direccion[0]
-            nueva_columna = jugador_pos[1] + direccion[1]
-            if 0 <= nueva_fila < alto and 0 <= nueva_columna < ancho and laberinto[nueva_fila, nueva_columna] != 1:
-                break
+        posibles_direcciones = [
+            (jugador_pos[0] + direccion[0], jugador_pos[1] + direccion[1])
+            for direccion in direcciones
+            if 0 <= jugador_pos[0] + direccion[0] < alto and 
+               0 <= jugador_pos[1] + direccion[1] < ancho and 
+               laberinto[jugador_pos[0] + direccion[0], jugador_pos[1] + direccion[1]] != 1
+        ]
+
+        if posibles_direcciones:
+            nueva_fila, nueva_columna = random.choice(posibles_direcciones)
     
     jugador_pos[0], jugador_pos[1] = nueva_fila, nueva_columna
     visitadas.add((nueva_fila, nueva_columna))
+
     canvas.coords(jugador,
                   jugador_pos[1] * tamano + 2, jugador_pos[0] * tamano + 2,
                   jugador_pos[1] * tamano + tamano - 2, jugador_pos[0] * tamano + tamano - 2)
 
+    if nueva_fila == fila_salida and nueva_columna == columna_salida:
+        messagebox.showinfo("Salida", "¡Has llegado a la salida!")
+        return  #se detiene si llega a la salida
+    
     if laberinto[nueva_fila, nueva_columna] == 111:
         manejar_trivia()
 
     if laberinto[nueva_fila, nueva_columna] == 3:
         manejar_teletransporte()
 
-    w.after(300, mover_jugador_auto)  # Mover cada 300 ms (lento)
+    w.after(50, mover_jugador_auto)
 
 mover_jugador_auto()
 
